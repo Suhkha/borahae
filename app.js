e90e726378555d5c0cc284b1fce3e31c8d1b3385
Jessica Villa <suhkha92@gmail.com>
2023-01-09 21:48:45 -0600

    First attempt to basic call to spotify

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const express = require('express');
+const express = require("express");
+const queryString = require("node:querystring");
+const axios = require("axios");
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-app.get("/", (request, response) => {
-    response.status(200).json({ message: "Hello from Express Server!" })
-});
-
 app.listen(PORT);
 
+app.get("/", (request, response) => {
+    response.send(
+        "<a href='https://accounts.spotify.com/authorize?client_id=" +
+        process.env.CLIENT_ID +
+        "&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Faccount&scope=user-top-read'>Sign in</a>"
+    );
+});
+
+app.get("/account", async(request, response) => {
+    const spotifyResponse = await axios.post(
+        "https://accounts.spotify.com/api/token",
+            queryString.stringify({
+            grant_type: "authorization_code",
+            code: request.query.code,
+            redirect_uri: process.env.REDIRECT_URI_DECODED,
+        }),
+        {
+            headers: {
+                Authorization: "Basic " + process.env.BASE64_AUTHORIZATION,
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        }
+    );
+
+    console.log(spotifyResponse.data);
+    const data = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=50", {
+        headers: {
+            Authorization: "Bearer " + spotifyResponse.data.access_token,
+        },
+    });
+
+});
