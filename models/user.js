360cdc48ae3ca35aaf35e562bdea5fe35b9e1657
Jessica Villa <suhkha92@gmail.com>
2023-04-11 09:34:07 -0600

    setup user schema

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+  nickname: {
+    type: String,
+    required: [true, "nickname is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  age: {
+    type: Number,
+  },
+  city: {
+    type: String,
+  },
+  social_media_url: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["ADMIN", "USER"],
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...user } = this.toObject();
+  user.uid = _id;
+
+  return user;
+};
+
+module.exports = model("Users", UserSchema);
