2185437545bba547103300d07881df92441a2b55
Jessica Villa <suhkha92@gmail.com>
2023-04-11 08:35:40 -0600

    setup database connection with mongoose

--- models/server.js
+++ models/server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const { databaseConnection } = require("../database/config");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
 
+    //database
+    this.database();
+
     //middleware
     this.middlewares();
 
     //routes
     this.routes();
+  }
+
+  async database() {
+    await databaseConnection();
   }
 
   middlewares() {
