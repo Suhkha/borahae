661d0e9a322e2ec5867e614a9eae16a21301950b
Jessica Villa <suhkha92@gmail.com>
2023-04-12 11:30:43 -0600

    auth for users and get x-token

---
+++ controllers/auth.js
@@ -0,0 +1,48 @@
+const { response } = require("express");
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+const { generateJWT } = require("../helpers/generate-jwt");
+
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({
+        message: "Something wrong with email or password --email",
+      });
+    }
+
+    //Check if user is active in database
+    if (!user.status) {
+      return res.status(400).json({
+        message: "Something wrong with email or password --status: false",
+      });
+    }
+
+    //Validate password
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(500).json({
+        message: "Something wrong with email or password --password",
+      });
+    }
+
+    // JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something wrong",
+    });
+  }
+};
+
+module.exports = {
+  login,
+};
