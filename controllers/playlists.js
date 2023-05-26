26b23adff7ad6639acc4c813cf8bb61cb1aca8a8
Jessica Villa <suhkha92@gmail.com>
2023-05-26 13:26:13 -0600

    create playlist in database separated from spotify logic

---
+++ controllers/playlists.js
@@ -0,0 +1,26 @@
+const { response } = require("express");
+const { Playlist } = require("../models");
+
+/**
+ * This allow to create the playlist in database only
+ */
+const createPlaylistDatabase = async (req, res = response) => {
+  const { name, description, userId } = req.body;
+
+  try {
+    const playlist = new Playlist({
+      name,
+      description,
+      userId,
+    });
+    await playlist.save();
+
+    res.json(playlist);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  createPlaylistDatabase,
+};
--- routes/playlists.js
+++ routes/playlists.js
@@ -1,24 +1,24 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validateFields } = require("../middlewares");
-const { createPlaylist } = require("../controllers/spotify-playlist");
-const { addTracksToPlaylist } = require("../controllers/spotify-tracks");
+const { createPlaylistDatabase } = require("../controllers/playlists");
+// const { addTracksToPlaylist } = require("../controllers/tracks");
 const router = Router();
 
 router.post(
-  "/create-playlist",
+  "/",
   [
     check("name", "please add a playlist name").not().isEmpty(),
     check("userId", "please add valid user id").isMongoId(),
     validateFields,
   ],
-  createPlaylist
+  createPlaylistDatabase
 );
 
-router.put(
-  "/add-tracks-to-playlist/:id",
-  [check("id", "invalid Mongo ID").isMongoId(), validateFields],
-  addTracksToPlaylist
-);
+// router.put(
+//   "/add-tracks-to-playlist/:id",
+//   [check("id", "invalid Mongo ID").isMongoId(), validateFields],
+//   addTracksToPlaylist
+// );
 
 module.exports = router;
